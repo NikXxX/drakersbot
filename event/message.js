2745ece1d09f5882bd63a1d9182c480ff6572dde
NikXxX <52099393+NikXxX@users.noreply.github.com>
2019-10-12 16:52:39 +0400

    Add files via upload

--- event/message.js
+++ event/message.js
@@ -6,16 +6,7 @@ module.exports = async (client, message) => {
       prefixes: "*"
     };
   }
-  let setwelcome = JSON.parse(fs.readFileSync('./welcome.json', 'utf8'));
-  if (!setwelcome[message.guild.id]) {
-    setwelcome[message.guild.id] = {
-      channel: null,
-      welcomemessage: null
-    };
-  }
-  const wc = setwelcome[message.guild.id].channel;
- wm = setwelcome[message.guild.id].welcomemessage;
-prefix = prefixes[message.guild.id].prefixes;
+const prefix = prefixes[message.guild.id].prefixes;
 
 
   if (message.author.bot || message.channel.type === "dm") return;
